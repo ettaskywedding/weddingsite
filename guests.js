411f3dfa9221011651ff539b190a747dc4966763
ettaskywedding <annalise.w.lasky@gmail.com>
2026-07-18 10:46:36 -0500

    Update guests.js
    added more guests

--- guests.js
+++ guests.js
@@ -1,5 +1,9 @@
 const GUESTS = [
 { name: "Jane Smith", seats: 3 },
+{ name: "Bobby Jurewicz", seats: 2 },
+{ name: "Pam King", seats: 1 },
+{ name: "Ethan Colish", seats: 2 },
+{ name: "Rayyan Qazi", seats: 2 },
 { name: "Alessondra Hopman", seats: 2 },
 { name: "Harrison Fox", seats: 1 },
 { name: "Lenny & Diane", seats: 2 },
